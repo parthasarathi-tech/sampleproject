0c612d29dd843d4ea8796bcbbc65bc05e24bd88c
Monsura <abc@gmail.com>
2023-05-17 16:03:48 +0530

    first commit

---
+++ demonew.java
@@ -0,0 +1,6 @@
+public static void main(){
+system.out.println("hello world");
+system.out.println("partha world");
+system.out.println("partha world");
+system.out.println("partha world");
+system.out.println("partha world")partha
